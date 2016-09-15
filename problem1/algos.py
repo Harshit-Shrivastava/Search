b434f5a8c5b1b13e76ee65c61b9ab1e0e9a9b30a
Pratik Jain <Pratik Jain>
2016-09-15 16:02:15 -0400

    Skeleton added

--- problem1/algos.py
+++ problem1/algos.py
@@ -1,3 +1,23 @@
+"""
+routing_options =
+- segments finds a route with the fewest number of \turns" (i.e. edges of the graph)
+- distance finds a route with the shortest total distance
+- time finds the fastest route, for a car that always travels at the speed limit
+- scenic finds the route having the least possible distance spent on highways (which we define
+as roads with speed limits 55 mph or greater)
+"""
+def best_path(graph, algo, start_city, end_city, routing_options):
+    if algo.lower() == "dfs":
+        return dfs(graph, start_city, end_city, routing_options);
+    elif algo.lower() == "bfs":
+        return bfs(graph, start_city, end_city, routing_options);
+    elif algo.lower() == "ids":
+        return ids(graph, start_city, end_city, routing_options);
+    elif algo.lower() == "astar":
+        return a_star(graph, start_city, end_city, routing_options);
+    else:
+        return False
+
 def dfs(graph, start_city, end_city, routing_options):
     stack = [(start_city, [start_city])]
     visited = set()
@@ -9,3 +29,35 @@ def dfs(graph, start_city, end_city, routing_options):
             visited.add(city)
             for next_city in graph[city]:
                 stack.append((next_city.end_city.name, path + [next_city.end_city.name]))
+
+def bfs(graph, start_city, end_city, routing_options):
+    return
+
+def ids(graph, start_city, end_city, routing_options):
+    return
+
+def a_star(graph, start_city, end_city, routing_options):
+    h = 0
+    if routing_options == "segments":
+        h = h_segments()
+    elif routing_options == "time":
+        h = h_time()
+    elif routing_options == "distance":
+        h = h_distance()
+    elif routing_options == "scenic":
+        h = h_scenic()
+    else:
+        return False
+    return
+
+def h_segments():
+    return
+
+def h_time():
+    return
+
+def h_distance():
+    return
+
+def h_scenic():
+    return
--- problem1/route.py
+++ problem1/route.py
@@ -20,13 +20,13 @@
 """
 import sys
 from utils_graph import build_graph
-from algos import dfs
+from algos import best_path
 
 
 if __name__ == "__main__":
     # start_city, end_city, routing_options, routing_algorithm = sys.argv[1:]
     graph, cities = build_graph()
     #print (graph,cities)
-    print (dfs(graph,"A","X",""))
+    print (best_path(graph,"dfs","A","X",""))
 
     # main(start_city, end_city, routing_options, routing_algorithm)
