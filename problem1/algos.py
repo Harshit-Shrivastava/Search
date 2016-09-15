1c357f0c22275ca0f4162d62e50f4a527a21aa09
Pratik Jain <Pratik Jain>
2016-09-15 17:56:46 -0400

    initial code - AStar

--- problem1/algos.py
+++ problem1/algos.py
@@ -6,6 +6,7 @@ routing_options =
 - scenic finds the route having the least possible distance spent on highways (which we define
 as roads with speed limits 55 mph or greater)
 """
+
 def best_path(graph, algo, start_city, end_city, routing_options):
     if algo.lower() == "dfs":
         return dfs(graph, start_city, end_city, routing_options);
@@ -37,6 +38,8 @@ def ids(graph, start_city, end_city, routing_options):
     return
 
 def a_star(graph, start_city, end_city, routing_options):
+    if (start_city == end_city):
+        return [start_city]
     h = 0
     if routing_options == "segments":
         h = h_segments()
@@ -48,6 +51,14 @@ def a_star(graph, start_city, end_city, routing_options):
         h = h_scenic()
     else:
         return False
+    fringe = []
+    min = float('inf')
+    for next_city in graph(start_city):
+        if min > next_city.distance:
+            min = next_city.distance
+            min_city = next_city.end_city.name
+        fringe.append(next_city.end_city.name,next_city.distance)
+
     return
 
 def h_segments():
@@ -56,7 +67,8 @@ def h_segments():
 def h_time():
     return
 
-def h_distance():
+def h_distance(cityA, cityB):
+
     return
 
 def h_scenic():
