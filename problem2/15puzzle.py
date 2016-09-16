70c787f4f6e4279ecc6a91a9e9befed75295ac9a
Harshit-Shrivastava <harshit.shrivastava14@gmail.com>
2016-09-16 16:05:29 -0400

    Barebone for problem2

---
+++ problem2/15puzzle.py
@@ -0,0 +1,6 @@
+#This is a classic 15-puzzle problem with one variation: When the empty tile is
+#on the edge of the board, the tile on the opposite side of the board can be slided into the opening.
+#The goal of the puzzle is to nd the shortest possible sequence of moves that restores the canonical configuration
+# (on the left above) given an initial board configuration.
+#The goal is to find a solution to this problem eciently using A* search
+#Input configuration is in the file input.txt
