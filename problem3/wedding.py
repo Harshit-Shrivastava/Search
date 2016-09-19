c6a8e3f52f86795666b61d9b50aff88a4427e6ef
Pratik Jain <Pratik Jain>
2016-09-19 11:33:30 -0400

    initial astar

--- problem3/wedding.py
+++ problem3/wedding.py
@@ -31,32 +31,14 @@ def compute_neighbours(friends, seats_per_table):
         for x in friends:
             if x != person and x not in friends[person]:
                 tentative_neighbours[person].add(x)
-
     tables = {}
-    i = 0
     for person in tentative_neighbours:
-        tables[i] = set(person)
-        # for neighbour in tentative_neighbours[person]:
-            # if
-        i += 1
-    """tables = {}
-    i=0
-    for neighbour in tentative_neighbours:
-        if len(tentative_neighbours[neighbour]) <= seats_per_table-1:
-            print(tentative_neighbours[neighbour],neighbour)
-            tables[i].add(tentative_neighbours[neighbour])
-            tables[i].add(neighbour)
-            i += 1
-
-    print(tables)"""
-    """for neighbour in tentative_neighbours:
-        tables[i] = set(neighbour)
-        for j in range( seats_per_table -1):
-            n = list(neighbour)[j]
-            tables[i].add( n )
-            tentative_neighbours[neighbour].remove(n)
-        i += 1"""
-    # return tables
+        tables[person] = []
+        for p in tentative_neighbours[person]:
+            for q in tentative_neighbours[person]:
+                if p in tentative_neighbours[q]:
+                    tables[person].append({p,q})
+    print(tables)
     return tentative_neighbours
 
 
